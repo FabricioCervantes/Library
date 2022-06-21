f9129e86614b37257d42f05fd6359a9ead6ad184
Fabricio-182 <marcofabri6@gmail.com>
2022-06-21 09:55:54 -0500

    Added delete and read status buttons to book card

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+let myLibrary = JSON.parse(localStorage.getItem("myLibrary") || "[]");
 
 function Book(title, author, pages, readStatus) {
     this.title = title;
@@ -7,51 +7,99 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus;
 }
 
-const book1 = new Book("1984", "George Orwell", "255", "Read");
-const book2 = new Book("It", "Stephen King", "1094 pages", "Not read");
+function addBookToLibrary() {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("readStatus");
+    if (read.checked) {
+        const book1 = new Book(title, author, pages, "Read");
+        myLibrary.push(book1);
+    } else {
+        const book1 = new Book(title, author, pages, "Not read");
+        myLibrary.push(book1);
+    }
 
 
-function formV() {
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+
+loadBooks();
+
+
+
+
+
+
+function deleteStorage() {
+    window.localStorage.clear();
+}
+
+function loadBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const bookCard = document.createElement("div");
+        const bookTitle = document.createElement("h4");
+        const bookAuthor = document.createElement("h5");
+        const bookPages = document.createElement("h5");
+        const bookReadBtn = document.createElement("div");
+        const deleteBtn = document.createElement("div");
+
+
+
+
+        bookCard.classList.add("card");
+        bookCard.setAttribute("id", i);
+        bookReadBtn.classList.add("read__btn");
+        document.querySelector(".card__container").appendChild(bookCard);
+        deleteBtn.classList.add("read__btn");
+
+        bookReadBtn.addEventListener("click", function() {
+            changeReadStatus(i);
+        });
+        deleteBtn.addEventListener("click", function() {
+            deleteBook(i);
+        });
+
+        bookTitle.textContent = myLibrary[i].title;
+        bookAuthor.textContent = myLibrary[i].author;
+        bookPages.textContent = myLibrary[i].pages + " pages";
+        bookReadBtn.textContent = myLibrary[i].readStatus;
+        deleteBtn.textContent = "Delete";
+
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPages);
+        bookCard.appendChild(bookReadBtn);
+        bookCard.appendChild(deleteBtn);
+    }
+}
+
+function changeReadStatus(bookId) {
+    if (myLibrary[bookId].readStatus == "Read") {
+        myLibrary[bookId].readStatus = "Not read";
+
+    } else {
+        myLibrary[bookId].readStatus = "Read";
+    }
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+    location.reload();
+}
+
+function deleteBook(bookId) {
+    const id = document.getElementById(bookId);
+    id.parentNode.removeChild(id);
+    myLibrary.splice(bookId, 1);
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+
+function showForm() {
     const form = document.getElementById('form');
 
     if (form.style.display === 'none') {
         // 👇️ this SHOWS the form
-        form.style.display = 'flexgit';
+        form.style.display = 'flex';
     } else {
         // 👇️ this HIDES the form
         form.style.display = 'none';
     }
-}
-
-console.log(book1.title);
-
-function addBookToLibrary() {
-    myLibrary.push(book1);
-    myLibrary.push(book2);
-}
-
-addBookToLibrary();
-
-for (let i = 0; i < myLibrary.length; i++) {
-    const bookCard = document.createElement("div");
-    const bookTitle = document.createElement("h4");
-    const bookAuthor = document.createElement("h5");
-    const bookPages = document.createElement("h5");
-    const bookReadBtn = document.createElement("div");
-
-
-    bookCard.classList.add("card");
-    bookReadBtn.classList.add("read__btn");
-    document.querySelector(".card__container").appendChild(bookCard);
-
-    bookTitle.textContent = myLibrary[i].title;
-    bookAuthor.textContent = myLibrary[i].author;
-    bookPages.textContent = myLibrary[i].pages + " pages";
-    bookReadBtn.textContent = myLibrary[i].readStatus;
-
-    bookCard.appendChild(bookTitle);
-    bookCard.appendChild(bookAuthor);
-    bookCard.appendChild(bookPages);
-    bookCard.appendChild(bookReadBtn);
-
 }
